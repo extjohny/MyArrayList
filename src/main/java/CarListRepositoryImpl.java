1acb8a6fd941404c300e590c92e4e8f49b23af9f
johny <verifykaq@gmail.com>
2025-04-26 21:35:06 +0300

    Create repo implementation

---
+++ src/main/java/CarListRepositoryImpl.java
@@ -0,0 +1,37 @@
+public class CarListRepositoryImpl implements CarListRepository {
+
+    @Override
+    public Car get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(Car car) {
+
+    }
+
+    @Override
+    public void add(Car car, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAt(int index) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
