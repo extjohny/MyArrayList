ffbbc80c5371ac7d346bd3fe61397e37a2a96b7f
johny <verifykaq@gmail.com>
2025-04-26 22:01:11 +0300

    Create JUnit4 Test class for repo

---
+++ src/test/java/CarListTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarListTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
